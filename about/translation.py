53418915832ea49334d80e8872e8abb69620b531
Dostonbek <tilavov.dostonbek@gmail.com>
2022-08-25 20:05:43 +0500

    feat: translation config for doctor model && handlers order config in tgbot/dispatcher

--- about/translation.py
+++ about/translation.py
@@ -1,15 +1,14 @@
 from modeltranslation.translator import translator, TranslationOptions
 from .models import Doctor
 
+
 class DoctorTranslationOptions(TranslationOptions):
-    fields = ('first_name', 
-            'last_name', 
-            'gender',
-            'content', 
-            'youtube', 
-            'telegram', 
-            'instagram', 
-            'twitter', 
-            'facebook')
+    fields = ('first_name',
+              'last_name',
+              'content',
+              'gender',
+              'youtube',
+              )
+
 
 translator.register(Doctor, DoctorTranslationOptions)
