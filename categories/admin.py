03e94a8e111a9caf2e32cccba0fb85d3a0254342
Abdulkhay_Payziev <abdulkhay.payziev@gmail.com>
2022-09-09 21:19:53 +0500

    fix:text field widget size

--- categories/admin.py
+++ categories/admin.py
@@ -1,8 +1,22 @@
+from django.db import models
+from django.forms import Textarea
 from django.contrib import admin
+
 from .models import Category, Condition, Question, Answer, Result
 
 admin.site.register(Category)
 admin.site.register(Condition)
-admin.site.register(Question)
-admin.site.register(Answer)
 admin.site.register(Result)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows': 10, 'cols': 60})},
+    }
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows': 4, 'cols': 60})},
+    }
